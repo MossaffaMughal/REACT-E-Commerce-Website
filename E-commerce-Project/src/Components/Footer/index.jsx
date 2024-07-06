55170982b485d650fe57fd5f62e0c23f24999284
MossaffaMughal <mossaffa.msm@gmail.com>
2024-07-06 14:29:52 +0500

    categories/functionality added

--- E-commerce-Project/src/Components/Footer/index.jsx
+++ E-commerce-Project/src/Components/Footer/index.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full flex bg-[#243049] text-white p-4 items-center justify-center">
+    <div className="flex w-full items-center justify-center bg-[#243049] p-4 text-white">
       <h1>All rights reserved</h1>
     </div>
   );
--- E-commerce-Project/src/Pages/Categories.jsx
+++ E-commerce-Project/src/Pages/Categories.jsx
@@ -1,5 +1,46 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Categories = () => {
-  return <h1>List of Categories</h1>;
+  const [categories, setCategories] = useState([]);
+  const navigate = useNavigate();
+
+  const showCategories = async () => {
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      const products = response.data;
+      const categoryList = [
+        ...new Set(products.map((product) => product.category.toUpperCase())),
+      ];
+      setCategories(categoryList);
+    } catch (e) {
+      console.log("Error: ", e);
+    }
+  };
+
+  useEffect(() => {
+    showCategories();
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="p-4 font-mono text-2xl">Categories</h1>
+      <ul>
+        {categories.map((category, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              navigate(`/products/${category.toLowerCase()}`);
+            }}
+            className="m-5 inline-block rounded-md p-4 shadow-md hover:cursor-pointer hover:border-blue-600 hover:bg-blue-400 hover:text-white"
+          >
+            {category}
+          </button>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Categories;
